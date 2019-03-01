03e043067c215c72b97e591e26aea75b05690d25
fedorchuksveta <fedrchuksveta@gmail.com>
2019-03-01 15:32:23 +0200

    Add file editProduct.jsp, changes made to header.jsp, addProduct.jsp, productInventory.jsp, home.jsp, main.css, carousel.css

--- src/main/java/com/emusicstore/controller/HomeController.java
+++ src/main/java/com/emusicstore/controller/HomeController.java
@@ -6,6 +6,7 @@ import com.emusicstore.model.Product;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,7 +14,9 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.multipart.MultipartFile;
 
 import javax.jws.WebParam;
+import javax.naming.Binding;
 import javax.servlet.http.HttpServletRequest;
+import javax.validation.Valid;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
@@ -78,7 +81,12 @@ public class HomeController {
     }
 
     @RequestMapping(value = "/admin/productInventory/addProduct", method = RequestMethod.POST)
-    public String addProductPost(@ModelAttribute("product") Product product, HttpServletRequest request) {
+    public String addProductPost(@Valid @ModelAttribute("product") Product product, BindingResult result, HttpServletRequest request) {
+
+       if(result.hasErrors()){
+           return "addProduct";
+       }
+
         productDao.addProduct(product);
 
         MultipartFile productImage = product.getProductImage();
@@ -113,9 +121,40 @@ public class HomeController {
 
         productDao.deleteProduct(id);
 
+        return "redirect:/admin/productInventory";
+    }
 
+    @RequestMapping("/admin/productInventory/editProduct/{id}")
+    public String editProduct(@PathVariable("id") String id, Model model){
+        Product product = productDao.getProductById(id);
 
+        model.addAttribute(product);
+
+        return "editProduct";
+    }
+
+    @RequestMapping(value = "/admin/productInventory/editProduct", method = RequestMethod.POST)
+    public String editProduct(@Valid @ModelAttribute("product") Product product, BindingResult result, Model model, HttpServletRequest request){
+
+        if(result.hasErrors()){
+            return "editProduct";
+        }
+
+        MultipartFile productImage = product.getProductImage();
+        String rootDirectory = request.getSession().getServletContext().getRealPath("/");
+        path = Paths.get(rootDirectory + "\\WEB-INF\\resources\\images\\" + product.getProductId() + ".png");
+
+        if(productImage != null && !productImage.isEmpty()){
+            try {
+                productImage.transferTo(new File(path.toString()));
+            }catch (Exception e){
+                throw new RuntimeException("Product image saving failed", e);
+            }
+        }
+
+        productDao.editProduct(product);
 
         return "redirect:/admin/productInventory";
     }
+
 }
