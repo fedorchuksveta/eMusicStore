b94509b1ae5236d32c9f2cf0ee6e53b520d8601c
fedorchuksveta <fedrchuksveta@gmail.com>
2019-03-20 15:29:59 +0200

    add Web Flow Pages

--- src/main/java/com/emusicstore/dao/impl/CartDaoImpl.java
+++ src/main/java/com/emusicstore/dao/impl/CartDaoImpl.java
@@ -3,6 +3,8 @@ package com.emusicstore.dao.impl;
 
 import com.emusicstore.dao.CartDao;
 import com.emusicstore.model.Cart;
+import com.emusicstore.model.CustomerOrder;
+import com.emusicstore.service.CustomerOrderService;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,21 +20,28 @@ public class CartDaoImpl implements CartDao {
     @Autowired
     private SessionFactory sessionFactory;
 
-    public Cart getCartById(int cartId){
+    @Autowired
+    private CustomerOrderService customerOrderService;
+
+    public Cart getCartById(int cartId) {
         Session session = sessionFactory.getCurrentSession();
         return (Cart) session.get(Cart.class, cartId);
     }
 
-    public void update(Cart cart){
+    public void update(Cart cart) {
 
         int cartId = cart.getCartId();
-        //to do later
+        double grandTotal = customerOrderService.getCustomerOrderGrandTotal(cartId);
+        cart.setGrandTotal(grandTotal);
+
+        Session session =sessionFactory.getCurrentSession();
+        session.saveOrUpdate(cart);
     }
 
-    public Cart validate(int cartId) throws IOException{
+    public Cart validate(int cartId) throws IOException {
         Cart cart = getCartById(cartId);
 
-        if(cart == null || cart.getCartItems().size() == 0){
+        if (cart == null || cart.getCartItems().size() == 0) {
             throw new IOException(cartId + "");
         }
 
